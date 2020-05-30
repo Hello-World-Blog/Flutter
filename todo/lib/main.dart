40b9a1eab1e4b4b0b35e475c7277b3eeb87c6d5c
devak997 <devakumar997@gmail.com>
2020-05-30 13:16:14 +0530

    refactored code

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/pages/add_a_task.dart';
-import 'package:todo/utils/database_provider.dart';
-import 'package:todo/utils/notification_provider.dart';
-import 'package:todo/widgets/task.dart';
 import 'package:flutter/services.dart';
+
+import 'providers/tasks_provider.dart';
+import './widgets/all_tasks.dart';
+import './widgets/completed_tasks.dart';
 
 void main() {
   runApp(MyApp());
@@ -35,17 +37,24 @@ List<String> months = [
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            iconTheme: IconThemeData(color: Color(0xff8280FF)),
-            primaryColor: Color(0xff8280FF),
-            backgroundColor: Colors.white,
-            fontFamily: "Segoe UI",
-            inputDecorationTheme: InputDecorationTheme(
-              border: UnderlineInputBorder(),
-            )),
-        home: ToDoHome());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: TasksProvider(),
+        )
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              iconTheme: IconThemeData(color: Color(0xff8280FF)),
+              primaryColor: Color(0xff8280FF),
+              backgroundColor: Colors.white,
+              fontFamily: "Segoe UI",
+              inputDecorationTheme: InputDecorationTheme(
+                border: UnderlineInputBorder(),
+              )),
+          home: ToDoHome()),
+    );
   }
 }
 
@@ -70,160 +79,83 @@ class _ToDoState extends State<ToDoHome> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     DateTime today = DateTime.now();
     return SafeArea(
-        child: Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: IconButton(
-        icon: Icon(Icons.add_circle_outline),
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => AddTask(),
-          ));
-        },
-        iconSize: 40,
-        color: Color(0xff8280FF),
-      ),
-      appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Text(
-            "Todo.",
-            style: TextStyle(
-                color: Color(0xff8280FF),
-                fontSize: 28,
-                fontWeight: FontWeight.bold),
-          ),
-          actions: [
-            Row(
-              children: [
-                Text(
-                  "${weeks[today.weekday - 1]}, ${months[today.month - 1]} ${today.day} ${today.year}",
-                  style: TextStyle(
-                      color: Colors.grey[700], fontWeight: FontWeight.w400),
-                ),
-                Icon(
-                      Icons.calendar_today,
-                      color: Color(0xff8280FF),
-                      size: 15,
-                    ),
-                SizedBox(
-                  width: 10,
-                )
-              ],
-            )
-          ],
-          bottom: TabBar(
-            controller: _tabController,
-            labelStyle: TextStyle(fontSize: 20),
-            indicatorColor: Color(0xff8280FF),
-            labelColor: Color(0xff8280FF),
-            unselectedLabelColor: Colors.grey[700],
-            tabs: [
-              Tab(
-                  child: Text(
-                "All",
-              )),
-              Tab(
-                  child: Text(
-                "Completed",
-              ))
-            ],
-          )),
-      body: TabBarView(controller: _tabController, children: [
-        Container(
-            padding: EdgeInsets.only(top: 10),
-            color: Colors.white,
-            child: FutureBuilder(
-                future: DatabaseProvider.db.getSortedtasks(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, i) {
-                          var item = snapshot.data[i];
-                          return Dismissible(
-                              confirmDismiss: (direction) =>
-                                  showDismissDialog(context, item),
-                              direction: DismissDirection.startToEnd,
-                              background: Container(
-                                color: Colors.red,
-                                child: Icon(
-                                  Icons.delete,
-                                  size: 30,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              key: ObjectKey(item),
-                              child: Task(item, notifyParent: refresh));
-                        });
-                  } else {
-                    return Center(
-                        child: CircularProgressIndicator(
-                            backgroundColor: Color(0xff8280FF)));
-                  }
-                })),
-        Container(
-            padding: EdgeInsets.only(top: 10),
-            color: Colors.white,
-            child: FutureBuilder(
-                future: DatabaseProvider.db.getSortedtasks(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, i) {
-                          var item = snapshot.data[i];
-                          if (snapshot.data[i].isCompleted == true) {
-                            return Task(
-                              item,
-                              notifyParent: refresh,
-                            );
-                          } else {
-                            return SizedBox();
-                          }
-                        });
-                  } else {
-                    return Center(
-                        child: CircularProgressIndicator(
-                            backgroundColor: Color(0xff8280FF)));
-                  }
-                })),
-      ]),
-    ));
-  }
-
-  Future<bool> showDismissDialog(context, item) async {
-    showDialog(
-      context: context,
-      child: AlertDialog(
-        actions: [
-          IconButton(
-              icon: Icon(Icons.check, color: Colors.green),
-              onPressed: () async {
-                await DatabaseProvider.db
-                    .delete(item.id)
-                    .then((value) => setState(() {}));
-                await NotificationProvider.instance.cancelNotification(item.id);
-                Navigator.pop(context);
-                return true;
-              }),
-          IconButton(
-            icon: Icon(
-              Icons.error,
-              color: Colors.red,
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: IconButton(
+          icon: Icon(Icons.add_circle_outline),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => AddTask(),
+            ));
+          },
+          iconSize: 40,
+          color: Color(0xff8280FF),
+        ),
+        appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            title: Text(
+              "Todo.",
+              style: TextStyle(
+                  color: Color(0xff8280FF),
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
             ),
-            onPressed: () {
-              Navigator.pop(context);
-              return false;
-            },
+            actions: [
+              Row(
+                children: [
+                  Text(
+                    "${weeks[today.weekday - 1]}, ${months[today.month - 1]} ${today.day} ${today.year}",
+                    style: TextStyle(
+                        color: Colors.grey[700], fontWeight: FontWeight.w400),
+                  ),
+                  Icon(
+                    Icons.calendar_today,
+                    color: Color(0xff8280FF),
+                    size: 15,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ],
+              )
+            ],
+            bottom: TabBar(
+              controller: _tabController,
+              labelStyle: TextStyle(fontSize: 20),
+              indicatorColor: Color(0xff8280FF),
+              labelColor: Color(0xff8280FF),
+              unselectedLabelColor: Colors.grey[700],
+              tabs: [
+                Tab(
+                    child: Text(
+                  "All",
+                )),
+                Tab(
+                    child: Text(
+                  "Completed",
+                ))
+              ],
+            )),
+        body: TabBarView(controller: _tabController, children: [
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            color: Colors.white,
+            child: Provider.of<TasksProvider>(context).isInit
+                ? AllTasks()
+                : Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Color(0xff8280FF),
+                    ),
+                  ),
           ),
-        ],
-        content: Text("Would you like to delete the Task ?"),
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            color: Colors.white,
+            child: CompletedTasks(),
+          ),
+        ]),
       ),
     );
-    return false;
-  }
-
-  void refresh() {
-    setState(() {});
   }
 }
