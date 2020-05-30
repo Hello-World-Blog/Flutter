dddbc2e26542a7a5d5094295df0a8b5092514129
devak997 <devakumar997@gmail.com>
2020-05-30 21:17:02 +0530

    global key added

---
+++ todo/lib/utils/globals.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+MyGlobals globals = new MyGlobals();
+
+class MyGlobals {
+  GlobalKey _scaffoldKey;
+  MyGlobals() {
+    _scaffoldKey = GlobalKey();
+  }
+  GlobalKey get scaffoldKey => _scaffoldKey;
+}
